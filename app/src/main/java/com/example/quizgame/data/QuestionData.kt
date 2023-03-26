c79c80a6c2673e85046610233aaa197c09817035
Adelerd <Flook.as2544@gmail.com>
2023-03-27 04:24:32 +0700

    add work

---
+++ app/src/main/java/com/example/quizgame/data/QuestionData.kt
@@ -0,0 +1,69 @@
+package com.example.quizgame.data
+
+
+class QuestionData {
+
+    val questionList = listOf(
+
+        Question(
+            "What is vegetarian food?",
+            listOf("cake", "pancake", "salad", "steak"),
+            "salad"
+        ),
+
+        Question(
+            "Which brand name is a sneaker brand?",
+            listOf("Yayoi", "Nike", "koi", "Audi"),
+            "Nike"
+        ),
+
+        Question(
+            "What should not be in the bathroom?",
+            listOf("hammer", "shampoo", "soap", "towel"),
+            "hammer"
+        ),
+
+        Question(
+            "What year is this year? ",
+            listOf("2033", "1988", "2022", "2023"),
+            "2023"
+        ),
+
+        Question(
+            "What is 54 plus 68?",
+            listOf("123", "122", "124", "125"),
+            "122"
+        ),
+
+        Question(
+            "Which band is not a Thai band?",
+            listOf("three man down", "cocktails", "maroon5", "paper plan"),
+            "maroon5"
+        ),
+
+        Question(
+            "Which name is not the name of the city?",
+            listOf("paris", "bangkok", "tokyo drift", "NewYork"),
+            "tokyo drift"
+        ),
+
+        Question(
+            "Who is not a singer?",
+            listOf("Rihanna", "selena gomez", "travis scott", "prayut"),
+            "prayut"
+        ),
+
+        Question(
+            "what is national dish of Italy?",
+            listOf("sushi", "hamburgers", "noodle", "spaghetti"),
+            "spaghetti"
+        ),
+
+        Question(
+            "What is a non-alcoholic drink?",
+            listOf("coconut milk", "sujo", "beer", "black label"),
+            "coconut milk"
+        ),
+
+        )
+}
